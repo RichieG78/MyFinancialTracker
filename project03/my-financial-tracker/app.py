0183743c7f2da87aec826a91d359b486a9fd293a
Richard Glynn <richardglynn@gmail.com>
2026-02-07 16:57:03 +0000

    Set up add expense and add income forms and pointed them to correct routes.

--- project03/my-financial-tracker/app.py
+++ project03/my-financial-tracker/app.py
@@ -5,5 +5,13 @@ app = Flask(__name__)
 def index():
     return render_template('dashboard.html')
 
+@app.route('/add-income', methods=['GET', 'POST'])
+def add_income():
+    return render_template('add-income.html')
+
+@app.route('/add-expense', methods=['GET', 'POST'])
+def add_expense():
+    return render_template('add-expense.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
